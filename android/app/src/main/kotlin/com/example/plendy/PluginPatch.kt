6454213fef906d42b6368a3f285d46d06599cc48
KPhamster <kevinphamster@gmail.com>
2025-03-09 18:28:48 -0700

    Added receive_sharing_intent plugin

---
+++ android/app/src/main/kotlin/com/example/plendy/PluginPatch.kt
@@ -0,0 +1,5 @@
+package com.example.plendy
+
+// This file is used to patch potential issues with plugins
+// It doesn't need to contain any actual code, but it ensures that
+// the Kotlin compiler settings for this project are applied
