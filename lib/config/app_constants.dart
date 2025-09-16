69c9814faf1d08bc8cea6d2ee2f80168690754b6
KPhamster <kagekevsama@gmail.com>
2025-09-16 00:20:16 -0700

    Share Experiences: implemented radio button options for sharing experiences

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -3,4 +3,6 @@ class AppConstants {
   static const String lastUsedCategoryKey = 'last_used_category_id';
   static const String lastUsedColorCategoryKey = 'last_used_color_category_id';
   static const String lastUsedOtherCategoriesKey = 'last_used_other_category_ids';
+  static const String lastShareModeKey = 'last_share_mode'; // 'my_copy' | 'separate_copy'
+  static const String lastShareGiveEditAccessKey = 'last_share_give_edit_access';
 } 
